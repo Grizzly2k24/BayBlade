ebc1ce32df13ee000dd41129bde228218314fb8f
Eidam1 <adamkrejci33@gmail.com>
2024-12-17 09:28:03 +0100

    Changing input system

--- Assets/pohyb.cs
+++ Assets/pohyb.cs
@@ -7,12 +7,15 @@ public class pohyb : MonoBehaviour
 {
     private Rigidbody rb;
     public float rotationSpeed = 100f; // Rychlost otáèení
+    PlayersInputMap inputActions;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         // Na zaèátku zamkni rotaci, aby byla pyramida stabilní
         rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        inputActions = new PlayersInputMap();
+        inputActions.Enable();
     }
 
     void Update()
@@ -21,8 +24,12 @@ public class pohyb : MonoBehaviour
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
 
         // Pohyb pyramidy pomocí šipek
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+
+        float moveHorizontal = inputActions.Player1.Movement.ReadValue<Vector2>().x;
+        float moveVertical = inputActions.Player1.Movement.ReadValue<Vector2>().y;
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * 10f); // Mùžeš upravit rychlost pohybu
